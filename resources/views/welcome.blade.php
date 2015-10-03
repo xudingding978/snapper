2ff880e3e5e0f950a145d91800f8bc9cf0188e26
kubydog <hcs1101@hotmail.com>
2015-10-03 16:16:53 +0800

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,261 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Snapper</title>
+        <!-- Bootstrap core CSS -->
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="css/navbar.css" rel="stylesheet">
+        <!-- Flexslider CSS-->
+        <link href="css/flexslider.css" rel="stylesheet">
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
     </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+    <body data-pg-collapsed>
+        <div class="header">
+            <!-- Static navbar -->
+            <div class="navbar navbar-default" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Snapper</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active">
+                            <a href="./">首页</a>
+                        </li>
+                        <li>
+                            <a href="../navbar-static-top/">我要出国</a>
+                        </li>
+                        <li>
+                            <a href="../navbar-fixed-top/">我的账户</a>
+                        </li>
+                        <li>
+                            <a href="../navbar-fixed-top/">帮助/反馈</a>
+                        </li>
+                    </ul>
+                </div>
+                <!--/.nav-collapse -->
+            </div>
+            <!-- Main component for a primary marketing message or call to action -->
+        </div>
+        <div class="container search-panel">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <input type="text" class="form-control searchBox" placeholder="Placeholder text">
+                    <a name="anchor" href="#" class="advance-link">高级搜索</a>
+                </div>
             </div>
         </div>
+        <div class="container flex-slider">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="flexslider"> 
+                        <ul class="slides"> 
+                            <li><img src="images/s1.jpg" /></li> 
+                            <li><img src="images/s2.jpg" /></li> 
+                            <li><img src="images/s3.jpg" /></li> 
+                            <li><img src="images/s4.jpg" /></li> 
+                        </ul> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container catagory">
+            <div class="row">
+                <div class="col-md-4 img">
+                    <img class="cat-img" src="images/p1.jpg"></img>
+                </div>
+                <div class="col-md-4 img">
+                    <img class="cat-img" src="images/p1.jpg"></img>
+                </div>
+                <div class="col-md-4 img">
+                    <img class="cat-img" src="images/p1.jpg"></img>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <section class="link-section">
+                        <div class="row">
+                            <p class="quick-type"><strong>留学国家</strong></p>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="aus" href="">澳大利亚</a>
+                                </div>
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="usa" href="">美国</a>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="nzl" href="">新西兰</a>
+                                </div>
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="eng" href="">英国</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <p class="quick-type"><strong>热门学校</strong></p>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="aus" href="">悉尼大学</a>
+                                </div>
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="usa" href="">墨尔本大学</a>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="nzl" href="">牛津大学</a>
+                                </div>
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="eng" href="">哈佛大学</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <p class="quick-type"><strong>热门业务</strong></p>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="aus" href="">留学申请</a>
+                                </div>
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="usa" href="">移民办理</a>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="nzl" href="">签证申请</a>
+                                </div>
+                                <div class="col-md-6">
+                                    <a class="quick-link" name="eng" href="">出国旅游</a>
+                                </div>
+                            </div>
+                        </div>
+                    </section>                     
+                </div>
+                <div class="col-md-7">
+                    <div class="panel"> 
+                        <div class="panel-body adv-panel">宣传页面</div>                         
+                    </div>
+                    <div class="push-links"> 
+                        <h4 style="padding-left:20px;">留学移民资讯</h4>
+                        <ul>
+                            <li>
+                                <a href="">澳洲技术移民打分新政</a>
+                            </li>
+                            <li>
+                                <a href="">澳洲技术移民打分新政</a>
+                            </li>
+                            <li>
+                                <a href="">澳洲技术移民打分新政</a>
+                            </li>
+                            <li>
+                                <a href="">澳洲技术移民打分新政</a>
+                            </li>
+                            <li>
+                                <a href="">澳洲技术移民打分新政</a>
+                            </li>
+                        </ul>                         
+                    </div>                     
+                </div>
+                <div class="col-md-3">
+                    <section class="login-section">
+                        <form class="form-horizontal" role="form">
+                            <legend class="legend">用户登录</legend>
+                            <div class="form-group"> 
+                                <label class="col-md-4 control-label login-label" for="username">用户名</label>
+                                <div class="col-md-8">                                     
+                                    <input type="text" class="form-control" id="username" placeholder="usernam">
+                                </div>                                     
+                            </div>                                 
+                            <div class="form-group"> 
+                                <label class="col-md-4 control-label login-label" for="password">密码</label>
+                                <div class="col-md-8">                                     
+                                    <input type="password" class="form-control" id="password" placeholder="Password">
+                                </div>                                     
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-10 col-md-offset-2">
+                                    <button type="submit" class="btn btn-sm quick-login">登录</button>
+                                    <button type="button" class="btn btn-sm quick-login">注册</button>
+                                </div>
+                            </div>                                 
+                        </form>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <div id="footer"> 
+            <div class="container">
+                <div class="col-md-4 link-1">
+                    <h4>关于</h4>
+                    <ul class="nav menu">
+                        <li class="item">
+                            <a href="">关于我们</a>
+                        </li>
+                        <li class="item">
+                            <a href="">版权声明</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4 link-2">
+                    <h4>联系</h4>
+                    <ul class="nav menu">
+                        <li class="item">
+                            <a href="">联系我们</a>
+                        </li>
+                        <li class="item-4">
+                            <a href="">在线咨询</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4 link-3">
+                    <h4>业务</h4>
+                    <ul class="nav menu">
+                        <li class="item">
+                            <a href="">联系我们</a>
+                        </li>
+                        <li class="item">
+                            <a href="">在线咨询</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>             
+        </div>          
+        <!-- /container -->
+        <!-- Bootstrap core JavaScript
+    ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="script/jquery-2.1.4.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script src="script/jquery.flexslider-min.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+        <script type="text/javascript">
+            $(function() {
+                $(".flexslider").flexslider({
+                    slideshowSpeed: 4000, //展示时间间隔ms
+                    animationSpeed: 400, //滚动时间ms
+                    touch: true //是否支持触屏滑动
+                });
+            }); 
+        </script>
     </body>
 </html>
